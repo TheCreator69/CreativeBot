988753c3d657197c906de8ff4a26d9c9e1539b59
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-21 17:18:40 +0100

    Changed method of executing commands

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
+for(const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
@@ -16,18 +16,23 @@ for(const file of commandFiles){
 
 
 client.once('ready', () => {
-    console.log('Codelyon is online!');
+    console.log("I'm online now!");
 });
 
-client.on('message', message =>{
+client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
+    for(const possibleCommand of client.commands) {
+        if(command == possibleCommand.commandName) {
+            client.commands.get(possibleCommand.commandName).execute(message, args);
+        }
     }
+    //if(command === 'ping') {
+        //client.commands.get('ping').execute(message, args);
+    //}
 });
 
 client.login('***REMOVED***');
