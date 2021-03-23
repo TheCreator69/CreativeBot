a6844be1f76f962f4ec0b18a0cc058c34e1622f9
SomeVeryCoolSkeleton <80484702+SomeVeryCoolSkeleton@users.noreply.github.com>
2021-03-23 17:01:57 +0000

    Creator (#2)
    * Added an image command called "rekt"
    
    * Included MessageAttachment
    
    Co-authored-by: TheCreator69 <16518527+TheCreator69@users.noreply.github.com>

---
+++ commands/rekt.js
@@ -0,0 +1,10 @@
+const { MessageAttachment } = require("discord.js");
+
+module.exports = {
+    name: 'rekt',
+    description: "Force the bot to send a wonderful image.",
+    execute(message, args) {
+        const attachment = new MessageAttachment("http://www.nag.co.za/wp-content/uploads/2014/09/rekt-mate.jpg");
+        message.channel.send(attachment);
+    }
+};
