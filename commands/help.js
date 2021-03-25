08a899c229dfbaec6ee67d1474ef1e82538d163f
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-25 19:21:11 +0100

    Added support for getting help for individual commands

--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,29 @@ module.exports = {
     name: 'help',
     description: "A standard help command! DMs the command sender with a list of possible commands.",
     execute(message, args) {
-        var helpMessage = "**List of possible commands:** \n";
-
         const Index = require("../index.js");
         const commandMap = Index.commandMap;
-        for(const [key, value] of commandMap) {
-            var commandObject = commandMap.get(key);
-            helpMessage = helpMessage.concat("**", commandObject.name, "** - ", commandObject.description, "\n");
-        }
+        var helpMessage = "";
 
+        if(args.length == 0) {
+            helpMessage = "**List of possible commands:** \n";
+
+            for(const [key, value] of commandMap) {
+                var commandObject = commandMap.get(key);
+                helpMessage = helpMessage.concat("**", commandObject.name, "** - ", commandObject.description, "\n");
+            }
+        }
+        else {
+            var requestedCommand = args[0];
+
+            if(commandMap.has(requestedCommand)) {
+                var commandObject = commandMap.get(requestedCommand);
+                helpMessage = helpMessage.concat("**", commandObject.name, "** - ", commandObject.description, "\n");
+            }
+            else {
+                helpMessage = "Sorry, but this is an invalid command :frowning:";
+            }
+        }
         message.author.send(helpMessage);
     }
 };
