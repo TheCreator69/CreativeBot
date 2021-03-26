722fcaa79336e4ba5f443433424f9284e29ebf5d
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-26 23:29:39 +0100

    Replaced "" with `` for require command to fix crash upon startup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 
 const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
 for(const file of commandFiles) {
-    const command = require("./commands/${file}");
+    const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
 }
