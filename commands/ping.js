24fddda770bd5d33e9553bf076a98919ac40b9fd
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-27 20:24:06 +0100

    Removed old commands to get ready for a competition!

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Respond with a 'Pong!' and an emote.",
-    syntax: "ping",
-    execute(message, args) {
-        message.channel.send(':ping_pong: Pong!');
-    }
-};
--- commands/reply.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "reply",
-    description: "Replies to the command sender with a nice message!",
-    syntax: "reply",
-    execute(message, args) {
-        message.reply('Thank you for talking to me :smile:');
-    }
-};
