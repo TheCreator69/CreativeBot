2d4bf1afa044f206ffc7ad039b7020b10de013a0
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-29 02:45:19 +0200

    Added slot machine command

---
+++ commands/slots.js
@@ -0,0 +1,19 @@
+var rollingMessage = "Rolling";
+var doneMessage = "Done! You win ";
+var slotMachineText = "\n - - - - - - - - -\n|                      |\n|   X    X    X   |\n|                      |\n - - - - - - - - -";
+var rollTimeMS = 4000;
+
+module.exports = {
+    name: "slots",
+    description: "Simulates a slot machine, but without the monetary losses...and gains.",
+    syntax: "slots",
+    execute(message, args) {
+        var coinsWon = Math.floor(Math.random() * 2000);
+
+        sentMessage = message.channel.send(rollingMessage + slotMachineText);
+        setTimeout(() => sentMessage.then(function(message) {message.edit(rollingMessage + "." + slotMachineText)}), 1000);
+        setTimeout(() => sentMessage.then(function(message) {message.edit(rollingMessage + ".." + slotMachineText)}), 2000);
+        setTimeout(() => sentMessage.then(function(message) {message.edit(rollingMessage + "..." + slotMachineText)}), 3000);
+        setTimeout(() => sentMessage.then(function(message) {message.edit(doneMessage + coinsWon + " coins!" + slotMachineText)}), 4000);
+    }
+};
