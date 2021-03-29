9465f93707ad05712978177ea0e5fbf14b94ee7a
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-29 22:51:44 +0200

    Added support for command category folders

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const prefix = config.prefix;
 client.commands = new Discord.Collection();
 const fs = require("fs");
 
-const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+const commandFolders = fs.readdirSync("./commands");
+for(const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+    for(const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
 
-    client.commands.set(command.name, command);
+        client.commands.set(command.name, command);
+    }
 }
-
 
 client.once("ready", () => {
     console.log("I'm online now!");
