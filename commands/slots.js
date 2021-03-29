0808110be0df2e98a8c561710207ab1cc6c11814
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-29 22:45:06 +0200

    Added win tiers and misc. improvements

--- commands/slots.js
+++ commands/slots.js
@@ -1,38 +1,36 @@
-var rollingMessage = "Rolling";
-var doneMessage = "Done! You win ";
 var rollCycles = 3;
 var symbols = [":grapes:", ":cherries:", ":lemon:", ":tangerine:", ":apple:", ":blueberries:", ":kiwi:", ":strawberry:"];
 
 module.exports = {
     name: "slots",
     description: "Simulates a slot machine, but without the monetary losses...and gains.",
-    syntax: "slots <bet amount>",
+    syntax: "slots",
     execute(message, args) {
-        var slotMessagePromise = message.channel.send(rollingMessage).then(function(message) {return message});
+        var slotMessagePromise = message.channel.send("Rolling").then(function(message) {return message});
         const waitForMessage = () => {
             slotMessagePromise.then((slotMessage) => {
-                //animateRollingMessage(slotMessage);
+                animateRollingMessage(slotMessage);
             });
         };
         waitForMessage();
         setTimeout(() => slotMessagePromise.then(function(slotMessage) {
             var randomSymbols = selectRandomSymbols();
-            var coinsWon = calculateWin(randomSymbols);
+            var coinsWon = calculateWin();
             finishAnimation(slotMessage, randomSymbols, coinsWon);
-        }), 0); //rollCycles * 4000 + 1000
+        }), rollCycles * 4000 + 1000);
     }
 };
 
 async function animateRollingMessage(slotMessage) {
     for(let i = 0; i < rollCycles; i++) {
         await sleep(1000);
-        slotMessage.edit(rollingMessage + ".");
+        slotMessage.edit("Rolling.");
         await sleep(1000);
-        slotMessage.edit(rollingMessage + "..");
+        slotMessage.edit("Rolling..");
         await sleep(1000);
-        slotMessage.edit(rollingMessage + "...");
+        slotMessage.edit("Rolling...");
         await sleep(1000);
-        slotMessage.edit(rollingMessage);
+        slotMessage.edit("Rolling");
     }
 }
 
@@ -48,13 +46,34 @@ function selectRandomSymbols() {
     return randomSymbols;
 }
 
-function calculateWin(randomSymbols) {
-    return Math.floor(Math.random() * 2000);
+function calculateWin() {
+    var winTier = Math.floor(Math.random() * 100) + 1;
+    if(winTier <= 50) {
+        return 0;
+    }
+    else if(winTier <= 75) {
+        return Math.floor(Math.random() * 400) + 350;
+    }
+    else if(winTier <= 87) {
+        return Math.floor(Math.random() * 750) + 750;
+    }
+    else if(winTier <= 96) {
+        return Math.floor(Math.random() * 1500) + 1500;
+    }
+    else {
+        return Math.floor(Math.random() * 3000) + 3000;
+    }
 }
 
 function finishAnimation(slotMessage, randomSymbols, coinsWon) {
     var slotMachineBorder = " - - - - - - - - -\n";
-    var finishedSlotText = doneMessage + coinsWon + " coins!\n" + slotMachineBorder;
+    var finishedSlotText = "";
+    if(coinsWon == 0) {
+        finishedSlotText = "Unfortunately, you won nothing :frowning:\nBetter luck next time!\n" + slotMachineBorder;
+    }
+    else {
+        finishedSlotText = "You've won " + coinsWon + " coins! Hooray!\n" + slotMachineBorder;
+    }
     for(let i = 0; i <= 8; i = i + 3) {
         var slotMachineRow = "  " + randomSymbols[i] + " " + randomSymbols[i+1] + " " + randomSymbols[i + 2] + "  \n";
         finishedSlotText = finishedSlotText + slotMachineRow;
