64669f8cadb7c995f7f79d3c1b176ae09ad25ede
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-30 23:07:03 +0200

    Added support for displaying text and typing in your own

--- commands/fun/obama.js
+++ commands/fun/obama.js
@@ -1,11 +1,33 @@
 const {MessageAttachment} = require("discord.js");
+const Canvas = require("canvas");
+
+//TODO: Center the string, adaptive text size
 
 module.exports = {
     name: "obama",
-    description: "Sends an inspiring Obama picture with custom text.",
-    syntax: "obama <text>",
-    execute(message, args) {
-        const attachment = new MessageAttachment("https://www.acclaimimages.com/_gallery/_free_images/0519-0906-1522-3927_president_barack_obama_points_at_you_o.jpg");
+    description: "Sends an inspiring Obama picture with an equally inspiring message (hopefully).",
+    syntax: "obama <message>",
+    async execute(message, args) {
+        const canvas = Canvas.createCanvas(3500, 2333);
+        const ctx = canvas.getContext("2d");
+
+        var obamaImage = await Canvas.loadImage("./obama.jpg");
+        ctx.drawImage(obamaImage, 0, 0, canvas.width, canvas.height);
+
+        ctx.font = "120px Arial";
+        ctx.fillStyle = "#000000";
+        if(!args.length) {
+            ctx.fillText("I see you didn't enter a message, shame on YOU!", 100, 400);
+        }
+        else {
+            var imageText = "";
+            for(var arg of args) {
+                imageText = imageText + arg + " ";
+            }
+            ctx.fillText(imageText, 100, 400);
+        }
+
+        var attachment = new MessageAttachment(canvas.toBuffer());
         message.channel.send(attachment);
     }
 };
