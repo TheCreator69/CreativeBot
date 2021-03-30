c15e21ed8527c543a43e6bfc30b161653db79475
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-30 23:32:41 +0200

    Adapted to changing image size and misc. changes

--- commands/fun/obama.js
+++ commands/fun/obama.js
@@ -1,6 +1,5 @@
 const {MessageAttachment} = require("discord.js");
 const Canvas = require("canvas");
-
 //TODO: Center the string, adaptive text size
 
 module.exports = {
@@ -8,26 +7,26 @@ module.exports = {
     description: "Sends an inspiring Obama picture with an equally inspiring message (hopefully).",
     syntax: "obama <message>",
     async execute(message, args) {
-        const canvas = Canvas.createCanvas(3500, 2333);
+        const canvas = Canvas.createCanvas(1200, 800);
         const ctx = canvas.getContext("2d");
 
-        var obamaImage = await Canvas.loadImage("./obama.jpg");
+        var obamaImage = await Canvas.loadImage("./obama.png");
         ctx.drawImage(obamaImage, 0, 0, canvas.width, canvas.height);
 
-        ctx.font = "120px Arial";
+        ctx.font = "50px Arial";
         ctx.fillStyle = "#000000";
         if(!args.length) {
-            ctx.fillText("I see you didn't enter a message, shame on YOU!", 100, 400);
+            ctx.fillText("I see you didn't enter a message, shame on YOU!", 50, 100);
         }
         else {
             var imageText = "";
             for(var arg of args) {
                 imageText = imageText + arg + " ";
             }
-            ctx.fillText(imageText, 100, 400);
+            ctx.fillText(imageText, 50, 100);
         }
 
-        var attachment = new MessageAttachment(canvas.toBuffer());
+        var attachment = new MessageAttachment(canvas.toBuffer(), "obama-is-very-inspiring.png");
         message.channel.send(attachment);
     }
 };
