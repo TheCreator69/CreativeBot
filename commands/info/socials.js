9314f35b54b4a443589b23aec3e99df598b0232d
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-03-31 14:01:55 +0200

    Finished refactoring command

--- commands/info/socials.js
+++ commands/info/socials.js
@@ -1,21 +1,25 @@
-const Discord = require("discord.js");
+const {MessageEmbed} = require("discord.js");
 
 module.exports = {
     name: "socials",
     description: "Lists ***REMOVED***'s socials in an embed.",
     syntax: "socials",
     execute(message, args) {
-        const testEmbed = new Discord.MessageEmbed();
-        testEmbed.setColor("#ff0000");
-        testEmbed.setTitle("Follow me on social media!");
-        testEmbed.setDescription("I post little to nothing, but seeing my follower count increase is incredibly satisfying.");
-        testEmbed.setAuthor("***REMOVED***", "https://lh3.googleusercontent.com/a-/AOh14GgyWf-iAzQRRgAVsacRifDpMN6_IOkV7w9Q1Lqu=s600-k-no-rp-mo", "https://www.youtube.com/channel/UC1U-tRxYC7sr1kd-Q92iyHA");
-        testEmbed.setThumbnail("https://lh3.googleusercontent.com/a-/AOh14GgyWf-iAzQRRgAVsacRifDpMN6_IOkV7w9Q1Lqu=s600-k-no-rp-mo");
-        testEmbed.addField("YouTube", "https://www.youtube.com/channel/UC1U-tRxYC7sr1kd-Q92iyHA", true);
-        testEmbed.addField("Twitter", "https://twitter.com/***REMOVED***1337_", true);
-        testEmbed.addField("Twitch", "https://www.twitch.tv/thecreator133769", true);
-        testEmbed.addField("Reddit", "https://www.reddit.com/user/***REMOVED***_1337", true);
-        testEmbed.setFooter("Please follow me, I need the gratification!");
-        message.channel.send(testEmbed);
+        message.channel.send(constructEmbed());
     }
 };
+
+function constructEmbed() {
+    const socialEmbed = new MessageEmbed();
+    socialEmbed.setColor("#ff0000");
+    socialEmbed.setTitle("Follow me on social media!");
+    socialEmbed.setDescription("I post little to nothing, but seeing my follower count increase is incredibly satisfying.");
+    socialEmbed.setAuthor("***REMOVED***", "https://lh3.googleusercontent.com/a-/AOh14GgyWf-iAzQRRgAVsacRifDpMN6_IOkV7w9Q1Lqu=s600-k-no-rp-mo", "https://www.youtube.com/channel/UC1U-tRxYC7sr1kd-Q92iyHA");
+    socialEmbed.setThumbnail("https://lh3.googleusercontent.com/a-/AOh14GgyWf-iAzQRRgAVsacRifDpMN6_IOkV7w9Q1Lqu=s600-k-no-rp-mo");
+    socialEmbed.addField("YouTube", "https://www.youtube.com/channel/UC1U-tRxYC7sr1kd-Q92iyHA", true);
+    socialEmbed.addField("Twitter", "https://twitter.com/***REMOVED***1337_", true);
+    socialEmbed.addField("Twitch", "https://www.twitch.tv/thecreator133769", true);
+    socialEmbed.addField("Reddit", "https://www.reddit.com/user/***REMOVED***_1337", true);
+    socialEmbed.setFooter("Please follow me, I need the gratification!");
+    return socialEmbed;
+}
