04ac4ab87be1accb5d0c84b22d84928d7d03bdc0
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-11 00:10:08 +0200

    Added a stop command for convenience

---
+++ commands/admin/stop.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "stop",
+    description: "Stops the NodeJS application entirely.",
+    syntax: "stop",
+    min_args: 0,
+    admin_only: true,
+    execute(message, args) {
+        process.exit();
+    }
+};
