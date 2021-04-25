155202aaf6d55a01bdd58477c8112945453b3d61
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-25 02:40:51 +0200

    Removed old tests

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -8,41 +8,6 @@ module.exports = {
     min_args: 0,
     admin_only: true,
     async execute(message, args) {
-        await testAdminScript(message);
-        //testCredits(message, args);
+        message.channel.send("Nothing to test right now...");
     }
 };
-
-async function testAdminScript(message) {
-    var isUserAdmin = await AdminScript.findOutIfUserIsAdmin(message.author.id);
-    console.log(isUserAdmin);
-}
-
-async function testCredits(message, args) {
-    const sequelize = new Sequelize("***REMOVED***", "***REMOVED***", "***REMOVED***", {
-        host: "localhost",
-        dialect: "mysql",
-        logging: false
-    });
-    const Credits = sequelize.define("credits", {
-        userID: {
-            type: Sequelize.BIGINT,
-            primaryKey: true
-        },
-        credits : {
-            type: Sequelize.INTEGER
-        }
-    },
-    {
-        timestamps: false
-    });
-    await Credits.sync();
-    const userEntry = await Credits.findOne({where: {userID: message.author.id}});
-    if(userEntry === null) {
-        console.log("Couldn't find ID in database! Creating new entry...");
-        var newUserEntry = await Credits.create({userID: message.author.id, credits: 0});
-    }
-    else {
-        console.log("Found ID in database! Your credits are currently: " + userEntry.credits);
-    }
-}
