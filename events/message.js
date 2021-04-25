bacccd1478e5e5750ae845df29d363a7956f1512
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-25 03:15:52 +0200

    Added support for new creditshandler script

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const config = require("../config.json");
 const Sequelize = require("sequelize");
 const AdminCheck = require("../scripts/admincheck.js");
+const CreditsHandler = require("../scripts/creditshandler.js");
 const invalidCommandMessage = "Sorry, but this command is invalid :frowning:";
 
 module.exports = {
@@ -16,37 +17,7 @@ module.exports = {
 };
 
 async function handleCreativeCredits(message) {
-    const sequelize = new Sequelize("***REMOVED***", "***REMOVED***", "***REMOVED***", {
-        host: "localhost",
-        dialect: "mysql",
-        logging: false
-    });
-    const Credits = sequelize.define("credits", {
-        userID: {
-            type: Sequelize.BIGINT,
-            primaryKey: true
-        },
-        credits: {
-            type: Sequelize.INTEGER
-        }
-    },
-    {
-        timestamps: false
-    });
-    await Credits.sync();
-    const userEntry = await Credits.findOne({where: {userID: message.author.id}});
-    if(userEntry === null) {
-        var newUserEntry = await Credits.create({userID: message.author.id, credits: 0});
-    }
-    else {
-        var newCredits = userEntry.credits + 5;
-        newCredits = newCredits.toString();
-        await Credits.update({
-            credits: newCredits
-        }, {
-            where: {userID: message.author.id}
-        });
-    }
+    CreditsHandler.incrementCreditsForUser(message.author.id, 5);
 }
 
 async function executeCommand(message, client) {
