d57089465d50969fbe2214647f1d3ee896f8903b
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-25 03:07:41 +0200

    Moved credit handling code to separate script

---
+++ scripts/creditshandler.js
@@ -0,0 +1,60 @@
+const Sequelize = require("sequelize");
+
+module.exports = {
+    async getCreditsForUser(userID) {
+        const sequelize = establishDatabaseConnection();
+        const Credits = await defineAndSyncCreditsTableModel(sequelize);
+        const userEntry = await returnExistingEntryOrCreateNewOne(userID, Credits);
+        return userEntry.credits;
+    },
+    async updateCreditsForUser(userID, newCredits) {
+        const sequelize = establishDatabaseConnection();
+        const Credits = await defineAndSyncCreditsTableModel(sequelize);
+        await returnExistingEntryOrCreateNewOne(userID, Credits);
+        await updateUserCredits(userID, newCredits, Credits);
+    }
+};
+
+function establishDatabaseConnection() {
+    return new Sequelize("***REMOVED***", "***REMOVED***", "***REMOVED***", {
+        host: "localhost",
+        dialect: "mysql",
+        logging: false
+    });
+}
+
+async function defineAndSyncCreditsTableModel(sequelize) {
+    const Credits = sequelize.define("credits", {
+        userID: {
+            type: Sequelize.BIGINT,
+            primaryKey: true
+        },
+        credits: {
+            type: Sequelize.INTEGER
+        }
+    },
+    {
+        timestamps: false
+    });
+    await Credits.sync();
+    return Credits;
+}
+
+async function returnExistingEntryOrCreateNewOne(userID, Credits) {
+    const userEntry = await Credits.findOne({where: {userID: userID}});
+    if(userEntry === null) {
+        return await Credits.create({userID: userID, credits: 0});
+    }
+    else {
+        return userEntry;
+    }
+}
+
+async function updateUserCredits(userID, newCredits, Credits) {
+    newCredits = newCredits.toString();
+    await Credits.update({
+        credits: newCredits
+    }, {
+        where: {userID: userID}
+    });
+}
