2f26330237da72cd946e1033ed7e2c7f9a024e6d
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-29 20:28:57 +0200

    Added formatting options like color and timestamps

--- scripts/logchamp.js
+++ scripts/logchamp.js
@@ -1,8 +1,15 @@
 const Winston = require("winston");
 
+const logFormat = Winston.format.printf(({level, message, timestamp}) => {
+    return `${timestamp} - ${level}: ${message}`;
+});
+
 const logger = Winston.createLogger({
     transports: [new Winston.transports.Console()],
-    format: Winston.format.simple(),
+    format: Winston.format.combine(
+        Winston.format.timestamp(),
+        Winston.format.colorize({all: true, colors: {"info": "white", "warn": "yellow", "error": "red"}}),
+        logFormat),
     exitOnError: false
 });
 
