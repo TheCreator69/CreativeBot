c55ef3ab897a373c5de1ff7288d147d49cbec974
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-30 02:45:42 +0200

    Added command for toggling event channels

---
+++ commands/events/toggleevent.js
@@ -0,0 +1,34 @@
+const EventHandler = require("../../scripts/eventhandler.js");
+
+module.exports = {
+    name: "toggleevent",
+    description: "Enable/Disable the event channel for this guild",
+    syntax: "toggleevent <enable/disable>",
+    min_args: 1,
+    admin_only: true,
+    async execute(message, args) {
+        if(!message.guild) {
+            message.channel.send("Please send this command in a server with an active event channel!");
+            return;
+        }
+        const channelEntry = await EventHandler.getEventChannelForGuild(message.guild.id);
+        if(channelEntry === null) {
+            message.channel.send("This Discord doesn't have an event channel!");
+            return;
+        }
+        if(args[0] === "enable") {
+            EventHandler.toggleEventChannel(message.guild.id, true);
+            message.channel.send("Event channel has been enabled!");
+            return;
+        }
+        else if(args[0] === "disable") {
+            EventHandler.toggleEventChannel(message.guild.id, false);
+            message.channel.send("Event channel has been disabled!");
+            return;
+        }
+        else {
+            message.channel.send("Please type either \"enable\" or \"disable\"!");
+            return;
+        }
+    }
+};
