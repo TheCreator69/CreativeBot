a81d198132b45bb0b8616d6354b6ba3745389573
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-04-30 15:58:06 +0200

    Added new command for setting an event channel in a server

---
+++ commands/events/setevent.js
@@ -0,0 +1,38 @@
+const EventHandler = require("../../scripts/eventhandler.js");
+
+module.exports = {
+    name: "setevent",
+    description: "Set a server's event channel or delete it.",
+    syntax: "setevent <channelID/delete>",
+    min_args: 1,
+    admin_only: true,
+    async execute(message, args) {
+        if(!message.guild || !message.guild.available) {
+            message.channel.send("Please send this command in an available server!");
+            return;
+        }
+        const serverID = message.guild.id;
+        const channelEntry = await EventHandler.getEventChannel(serverID);
+        if(channelEntry !== null && args[0] === "delete") {
+            EventHandler.deleteEventChannel(serverID);
+            message.channel.send("Deleted event channel!");
+            return;
+        }
+        if(isNaN(args[0])) {
+            message.channel.send("You need to enter a number as a channel ID!");
+            return;
+        }
+        if(message.guild.channels.cache.get(args[0]) === undefined) {
+            message.channel.send("Couldn't find channel by ID!");
+            return;
+        }
+        if(channelEntry === null) {
+            await EventHandler.createEventChannel(serverID, args[0]);
+            message.channel.send("Set up event channel!");
+            return;
+        }
+        await EventHandler.changeEventChannel(serverID, args[0]);
+        message.channel.send("Changed event channel!");
+        return;
+    }
+};
