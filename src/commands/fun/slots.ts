0ad171cdafde1f8ef3f0bde8c78002699e1b4cd0
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-11 13:29:42 +0200

    Fixed scaling of credits rank text

--- src/commands/fun/slots.ts
+++ src/commands/fun/slots.ts
@@ -1,27 +1,42 @@
 import {Message} from "discord.js";
+import * as CreditsHandler from "../../scripts/creditshandler";
 
-var rollCycles = 3;
-var symbols = [":grapes:", ":cherries:", ":lemon:", ":tangerine:", ":apple:", ":blueberries:", ":kiwi:", ":strawberry:"];
+var rollCycles = 0;
+var symbols = [":grapes:", ":cherries:", ":lemon:", ":green_apple:", ":kiwi:", ":peach:"];
 
 module.exports = {
     name: "slots",
-    description: "Simulates a slot machine, but without the monetary losses...and gains.",
-    syntax: "slots",
-    min_args: 0,
+    description: "Simulates a slot machine, but without the monetary losses...and gains. You win if there are horizontal or diagonal lines of matching symbols.",
+    syntax: "slots <bet>",
+    min_args: 1,
     admin_only: false,
     async execute(message: Message, args: string[]) {
-        await editMessageOverTime(message);
+        await editMessageIfBetIsValid(message, args);
     }
 };
 
-async function editMessageOverTime(message: Message): Promise<void> {
-    var slotMessage = await message.channel.send("Rolling");
-    animateMessageWhenReady(slotMessage);
-    finishAnimation(slotMessage);
+async function editMessageIfBetIsValid(message: Message, args: string[]): Promise<void> {
+    var bet = parseInt(args[0]);
+    if(isNaN(bet)) {
+        message.channel.send("Please enter a number as a bet!");
+        return;
+    }
+    if(bet === 0) {
+        message.channel.send("You know, the winning amount gets multiplied by your bet. And what is x * 0?");
+        return;
+    }
+    var creditsOfAuthor = await CreditsHandler.getCreditsForUser(BigInt(message.author.id));
+    if(bet > creditsOfAuthor) {
+        message.channel.send("Your bet exceeds the amount of credits you have!");
+        return;
+    }
+    createAndEditMessageOverTime(message, bet);
 }
 
-async function animateMessageWhenReady(slotMessage: Message): Promise<void> {
+async function createAndEditMessageOverTime(message: Message, bet: number): Promise<void> {
+    var slotMessage = await message.channel.send("Rolling");
     animateRollingMessage(slotMessage);
+    await finishAnimationAndAwardCoins(slotMessage, message, bet);
 }
 
 async function animateRollingMessage(slotMessage: Message): Promise<void> {
@@ -41,54 +56,81 @@ function sleep(delay: number): Promise<unknown> {
     return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
-function finishAnimation(slotMessage: Message): void {
+async function finishAnimationAndAwardCoins(slotMessage: Message, message: Message, bet: number): Promise<void> {
     setTimeout(() => {
-        var randomSymbols = selectRandomSymbols();
-        var coinsWon = calculateWin();
-        finishEditingMessage(slotMessage, randomSymbols, coinsWon);
+        var randomSymbols = selectSeeminglyRandomSymbols();
+        var creditsWon = determineCreditsWon(randomSymbols, bet);
+        finishEditingMessage(slotMessage, randomSymbols, creditsWon);
+        withdrawOrAwardCredits(message, creditsWon, bet);
     }, rollCycles * 4000 + 1000);
 }
 
-function selectRandomSymbols(): string[] {
+function selectSeeminglyRandomSymbols(): string[] {
     var randomSymbols: string[] = [];
-    for(let i = 0; i < 9; i++) {
-        randomSymbols.push(symbols[Math.floor(Math.random() * symbols.length)]);
+    for(let i = 0; i < 9; i++) { //Hard-coded stuff. Always a good idea.
+        if(i === 0 || i === 3 || i === 6) {
+            randomSymbols.push(symbols[Math.floor(Math.random() * symbols.length)]);
+        }
+        else {
+            if(Math.random() < 0.25) {
+                var differenceBetweenIndexOfFirstSymbolOnRow = Math.floor(i % 3); //Probably the best variable name in existance
+                randomSymbols.push(randomSymbols[i - differenceBetweenIndexOfFirstSymbolOnRow]);
+            }
+            else {
+                randomSymbols.push(symbols[Math.floor(Math.random() * symbols.length)]);
+            }
+        }
     }
     return randomSymbols;
 }
 
-function calculateWin(): number {
-    var winTier = Math.floor(Math.random() * 100) + 1;
-    if(winTier <= 50) {
-        return 0;
-    }
-    else if(winTier <= 75) {
-        return Math.floor(Math.random() * 400) + 350;
-    }
-    else if(winTier <= 87) {
-        return Math.floor(Math.random() * 750) + 750;
-    }
-    else if(winTier <= 96) {
-        return Math.floor(Math.random() * 1500) + 1500;
-    }
-    else {
-        return Math.floor(Math.random() * 3000) + 3000;
-    }
+function determineCreditsWon(randomSymbols: string[], bet: number): number {
+    var betMultiplier = getHorizontalRowCount(randomSymbols) * 2 + getDiagonalRowCount(randomSymbols) * 4;
+    return bet * betMultiplier;
 }
 
-function finishEditingMessage(slotMessage: Message, randomSymbols: string[], coinsWon: number): void {
+function getHorizontalRowCount(randomSymbols: string[]): number {
+    var numberOfHorizontalRows = 0;
+    for(let i = 0; i <= 8; i += 3) {
+        if(randomSymbols[i] === randomSymbols[i + 1] && randomSymbols[i] === randomSymbols[i + 2]) {
+            numberOfHorizontalRows++;
+        }
+    }
+    return numberOfHorizontalRows;
+}
+
+function getDiagonalRowCount(randomSymbols: string[]): number {
+    var numberOfDiagonalRows = 0;
+    if(randomSymbols[0] === randomSymbols[4] && randomSymbols[0] === randomSymbols[8]) {
+        numberOfDiagonalRows++;
+    }
+    if(randomSymbols[6] === randomSymbols[4] && randomSymbols[6] === randomSymbols[2]) {
+        numberOfDiagonalRows++;
+    }
+    return numberOfDiagonalRows;
+}
+
+function finishEditingMessage(slotMessage: Message, randomSymbols: string[], creditsWon: number): void {
     var slotMachineBorder = " - - - - - - - - -\n";
-    var finishedSlotText = "";
-    if(coinsWon == 0) {
-        finishedSlotText = "Unfortunately, you won nothing :frowning:\nBetter luck next time!\n" + slotMachineBorder;
+    var finishedSlotMessage = "";
+    if(creditsWon == 0) {
+        finishedSlotMessage += "Unfortunately, you lost your bet :frowning:\nBetter luck next time!\n" + slotMachineBorder;
     }
     else {
-        finishedSlotText = "You've won " + coinsWon + " coins! Hooray!\n" + slotMachineBorder;
+        finishedSlotMessage += "You've won " + creditsWon + " coins! Hooray!\n" + slotMachineBorder;
     }
-    for(let i = 0; i <= 8; i = i + 3) {
+    for(let i = 0; i <= 8; i += 3) {
         var slotMachineRow = "  " + randomSymbols[i] + " " + randomSymbols[i + 1] + " " + randomSymbols[i + 2] + "  \n";
-        finishedSlotText = finishedSlotText + slotMachineRow;
+        finishedSlotMessage += slotMachineRow;
     }
-    finishedSlotText = finishedSlotText + slotMachineBorder;
-    slotMessage.edit(finishedSlotText);
+    finishedSlotMessage += slotMachineBorder;
+    slotMessage.edit(finishedSlotMessage);
+}
+
+async function withdrawOrAwardCredits(message: Message, creditsWon: number, bet: number): Promise<void> {
+    if(creditsWon === 0) {
+        await CreditsHandler.incrementCreditsForUser(BigInt(message.author.id), -bet);
+        return;
+    }
+    await CreditsHandler.incrementCreditsForUser(BigInt(message.author.id), creditsWon - bet);
 }
