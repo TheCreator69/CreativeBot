ce735da324940b5174b80dedb31f20400c19bd52
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-12 14:47:43 +0200

    Complete code refactor

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,51 +1,84 @@
 import * as config from "../config.json";
 import * as AdminCheck from "../scripts/admincheck";
 import * as CreditsHandler from "../scripts/creditshandler";
-import {commandMap} from "../index";
+import {commands} from "../index";
 import {Message, Client} from "discord.js";
 
 module.exports = {
     name: "message",
-    async execute(message: Message, client: Client) {
-        if(process.env.NODE_ENV == "production") {
-            if(message.content.startsWith(config.prefix) && !message.author.bot) {
-                executeCommand(message);
-            }
-            else if(!message.author.bot) {
-                await handleCreativeCredits(message);
-            }
-        }
-        else if(process.env.NODE_ENV == "development" || process.env.NODE_ENV == "build") {
-            if(message.content.startsWith(config.prefix) && !message.author.bot && await AdminCheck.checkIfUserIsAdmin(BigInt(message.author.id))) {
-                executeCommand(message);
-            }
-        }
+    async execute(message: Message, client: Client): Promise<void> {
+        handleMessageInDifferentEnvironments(message);
     }
 };
 
-async function handleCreativeCredits(message: Message) {
+async function handleMessageInDifferentEnvironments(message: Message): Promise<void> {
+    if(process.env.NODE_ENV == "production") {
+        handleMessageInProduction(message);
+    }
+    else if(process.env.NODE_ENV == "development" || process.env.NODE_ENV == "build") {
+        handleMessageInDevAndBuild(message);
+    }
+}
+
+async function handleMessageInProduction(message: Message): Promise<void> {
+    if(message.content.startsWith(config.prefix) && !message.author.bot) {
+        executeCommand(message);
+    }
+    else if(!message.author.bot) {
+        await handleCreativeCredits(message);
+    }
+}
+
+async function handleMessageInDevAndBuild(message: Message): Promise<void> {
+    if(message.content.startsWith(config.prefix) && !message.author.bot && await AdminCheck.checkIfUserIsAdmin(BigInt(message.author.id))) {
+        executeCommand(message);
+    }
+}
+
+async function handleCreativeCredits(message: Message): Promise<void> {
     if(message.type === "DEFAULT") {
         await CreditsHandler.incrementCreditsForUser(BigInt(message.author.id), message.content.length);
     }
 }
 
-async function executeCommand(message: Message) {
+interface CommandInfo {
+    name: string;
+    args: string[];
+}
+
+async function executeCommand(message: Message): Promise<void> {
+    var commandInfo = getCommandInfoFromMessage(message);
+
+    if(await canCommandBeExecuted(message, commandInfo)) {
+        var command = commands.get(commandInfo.name);
+        command.execute(message, commandInfo.args);
+    }
+}
+
+function getCommandInfoFromMessage(message: Message): CommandInfo {
     const args = message.content.slice(config.prefix.length).split(/ +/);
 
-    var command = args.shift();
-    if(command === undefined) return;
-    command = command.toLowerCase();
+    var commandName = args.shift();
+    if(commandName === undefined) {
+        return {name: "", args: args};
+    }
+    commandName = commandName.toLowerCase();
 
-    if(!commandMap.has(command)) {
-        return;
+    return {name: commandName, args: args};
+}
+
+async function canCommandBeExecuted(message: Message, commandInfo: CommandInfo): Promise<boolean> {
+    if(!commands.has(commandInfo.name)) {
+        return false;
     }
-    var commandMapEntry = commandMap.get(command);
-    if(commandMapEntry.admin_only && !await AdminCheck.checkIfUserIsAdmin(BigInt(message.author.id))) {
-        return;
+    var command = commands.get(commandInfo.name);
+    if(command.admin_only && !await AdminCheck.checkIfUserIsAdmin(BigInt(message.author.id))) {
+        return false;
     }
-    if(args.length < commandMapEntry.min_args) {
-        message.channel.send("You need to provide the required arguments for the command to work! See `" + config.prefix + "help " + commandMapEntry.name + "` for details!");
-        return;
+    //Check is required args were entered, if not, help the user out and return
+    if(commandInfo.args.length < command.min_args) {
+        message.channel.send("You need to provide the required arguments for the command to work! See `" + config.prefix + "help " + command.name + "` for details!");
+        return false;
     }
-    commandMapEntry.execute(message, args);
+    return true;
 }
