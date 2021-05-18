ad76c05311bbd129f4044b65eb54b94cffcf7726
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-18 19:20:14 +0200

    Created function specifically for code sorting command array

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -22,21 +22,29 @@ export async function execute(message: Message, args: string[]): Promise<void> {
 
 function createCorrectHelpEmbed(message: Message, args: string[]): MessageEmbed {
     if(!args.length) {
-        return createHelpEmbed("#52ce7b", ":page_facing_up: List of possible commands:", listAllCommandsAlphabetically(message), "To learn more about individual commands, use " + config.prefix + "help [command]!");
+        return createHelpEmbed("#52ce7b",
+        ":page_facing_up: List of possible commands:",
+        listAllCommandsAlphabetically(message),
+        "To learn more about individual commands, use " + config.prefix + "help [command]!");
     }
     else {
         if(doesCommandExist(args)) {
-            return createHelpEmbed("#499fff", ":ledger: Help for: " + getCommandInstance(args).info.name, createCommandInfoString(args), "Arguments wrapped with \"<>\" are required, others wrapped with \"[]\" are optional.");
+            return createHelpEmbed("#499fff",
+            ":ledger: Help for: " + getCommandInstance(args).info.name,
+            createCommandInfoString(args),
+            "Arguments wrapped with \"<>\" are required, others wrapped with \"[]\" are optional.");
         }
         else {
-            return createHelpEmbed("#ff0000", ":interrobang: Invalid command!", "Sorry, but this command doesn't exist :frowning:", "");
+            return createHelpEmbed("#ff0000",
+            ":interrobang: Invalid command!",
+            "Sorry, but this command doesn't exist :frowning:");
         }
     }
 }
 
-function createHelpEmbed(color: string, title: string, description: string, footer: string): MessageEmbed {
+function createHelpEmbed(colorInHex: string, title: string, description: string, footer?: string): MessageEmbed {
     var helpEmbed = new MessageEmbed();
-    helpEmbed.setColor(color);
+    helpEmbed.setColor(colorInHex);
     helpEmbed.setTitle(title);
     helpEmbed.setDescription(description);
     helpEmbed.setFooter(footer);
@@ -44,17 +52,8 @@ function createHelpEmbed(color: string, title: string, description: string, foot
 }
 
 function listAllCommandsAlphabetically(message: Message): string {
+    var commands = sortCommandsAlphabetically(Index.commands.array());
     var commandList = "";
-    var commands = Index.commands.array();
-    commands.sort(function(a, b) {
-        if(a.info.name < b.info.name) {
-            return -1;
-        }
-        else if(a.info.name > b.info.name) {
-            return 1;
-        }
-        return 0;
-    });
     for(const commandObject of commands) {
         if(commandObject.info.admin_only) {
             commandList += listAdminCommandForAdminsOnlyInDM(message, commandObject);
@@ -65,6 +64,18 @@ function listAllCommandsAlphabetically(message: Message): string {
     }
     commandList = commandList.substr(0, commandList.length - 2);
     return commandList;
+}
+
+function sortCommandsAlphabetically(commands: any[]): any[] {
+    return commands.sort(function(a, b) {
+        if(a.info.name < b.info.name) {
+            return -1;
+        }
+        else if(a.info.name > b.info.name) {
+            return 1;
+        }
+        return 0;
+    });
 }
 
 function listAdminCommandForAdminsOnlyInDM(message: Message, commandObject: any): string {
