1de4f66186aab0570c7fdd56fafc6537e07a2a01
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-20 21:35:40 +0200

    Added interface for commands as a whole

--- src/scripts/commanddef.ts
+++ src/scripts/commanddef.ts
@@ -1,3 +1,5 @@
+import {Message} from "discord.js";
+
 export interface CreativeCommandAttributes {
     name: string,
     description: string,
@@ -5,4 +7,10 @@ export interface CreativeCommandAttributes {
     min_args: number,
     admin_only: boolean,
     category?: string,
+}
+
+export interface CreativeCommand {
+    info: CreativeCommandAttributes,
+    checkRequiredArgs: (message: Message, args: string[]) => boolean,
+    execute: (message: Message, args: string[]) => void
 }
