6b782d8101a2b9ce8106044eb079769fbff9909a
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-26 12:48:06 +0200

    Added CreativeEvent interface for all events

--- src/scripts/eventdef.ts
+++ src/scripts/eventdef.ts
@@ -1,4 +1,11 @@
+import {Client} from "discord.js";
+
 export interface EventAttributes {
     name: string,
     once?: boolean
+}
+
+export interface CreativeEvent {
+    info: EventAttributes,
+    execute: (client: Client, ...args: any[]) => void
 }
