21e22fbff7c5f0bce04caafadc0b6a74f1a1879b
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-05-29 23:53:22 +0200

    Added some more unit tests

--- src/commands/info/help.test.ts
+++ src/commands/info/help.test.ts
@@ -2,10 +2,17 @@ import * as Help from "./help";
 import {CreativeCommand} from "../../scripts/commanddef";
 import {Message, Collection} from "discord.js";
 
-test("Sorts an array with commands 'Apple' and 'Banana'. Expects 'Apple' to be at index 0", () => {
-    const mockCollection = new Collection<string, CreativeCommand>();
+const mockCollection = new Collection<string, CreativeCommand>();
+var helpCommand = new Help.HelpCommand(mockCollection);
+
+beforeAll(() => {
     mockCollection.set("Banana", {
         name: "Banana", description: "", syntax: "", min_args: 0, admin_only: false,
+        async checkRequiredArgs(message: Message, args: string[]): Promise<boolean> {return true;},
+        execute(message: Message, args: string[]): void {}
+    });
+    mockCollection.set("Cherry", {
+        name: "Cherry", description: "", syntax: "", min_args: 0, admin_only: true,
         async checkRequiredArgs(message: Message, args: string[]): Promise<boolean> {return true;},
         execute(message: Message, args: string[]): void {}
     });
@@ -14,9 +21,52 @@ test("Sorts an array with commands 'Apple' and 'Banana'. Expects 'Apple' to be a
         async checkRequiredArgs(message: Message, args: string[]): Promise<boolean> {return true;},
         execute(message: Message, args: string[]): void {}
     });
+    helpCommand = new Help.HelpCommand(mockCollection);
+});
 
-    var helpCommand = new Help.HelpCommand(mockCollection);
-
+test("Sorts an array with commands 'Apple', 'Banana' and 'Cherry'. Expects 'Apple' to be at index 0", () => {
     const sortedArray = helpCommand.sortCommandsAlphabetically(mockCollection.array());
     expect(sortedArray[0].name).toBe("Apple");
+});
+
+test("Sorts an array with commands 'Apple', 'Banana' and 'Cherry'. Expects 'Banana' to be at index 1", () => {
+    const sortedArray = helpCommand.sortCommandsAlphabetically(mockCollection.array());
+    expect(sortedArray[1].name).toBe("Banana");
+});
+
+test("Sorts an array with commands 'Apple', 'Banana' and 'Cherry'. Expects 'Cherry' to be at index 2", () => {
+    const sortedArray = helpCommand.sortCommandsAlphabetically(mockCollection.array());
+    expect(sortedArray[2].name).toBe("Cherry");
+});
+
+test("Checks if the 'Null' command exists. Expects result to be false", () => {
+    expect(helpCommand.doesCommandExist(["Null"])).toBeFalsy();
+});
+
+test("Checks if the Apple command exists when the player isn't an admin and the command isn't admin-only. Expects result to be true", () => {
+    expect(helpCommand.doesCommandExist(["Apple"])).toBeTruthy(); //admin_only = false
+});
+
+test("Checks if the Cherry command exists when the player isn't an admin and the command is admin-only. Expects result to be false", () => {
+    expect(helpCommand.doesCommandExist(["Cherry"])).toBeFalsy(); //admin_only = true
+});
+
+test("Checks if the Apple command exists when the player is an admin and the command isn't admin-only. Expects result to be true", () => {
+    helpCommand.isCommandSenderAdmin = true;
+    expect(helpCommand.doesCommandExist(["Apple"])).toBeTruthy(); //admin_only = false
+    helpCommand.isCommandSenderAdmin = false;
+});
+
+test("Checks if the Cherry command exists when the player is an admin and the command is admin-only. Expects result to be true", () => {
+    helpCommand.isCommandSenderAdmin = true;
+    expect(helpCommand.doesCommandExist(["Cherry"])).toBeTruthy(); //admin_only = true
+    helpCommand.isCommandSenderAdmin = false;
+});
+
+test("Attempts to get the command 'Null' from the list of commands and expects the return value to be undefined", () => {
+    expect(helpCommand.getCommandInstance(["Null"])).toBeUndefined();
+});
+
+test("Attempts to get the command 'Banana' from the list of commands and expects the return value not to be undefined", () => {
+    expect(helpCommand.getCommandInstance(["Banana"])).not.toBeUndefined();
 });
