cfd5343a1e7e481d5806aa303ecf01dc81489b5e
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-04 20:40:15 +0200

    Refactoring and added localizer init

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,7 @@ import {Client} from "discord.js";
 const config = require("../config.json");
 import * as LogChamp from "../scripts/logchamp";
 import {EventAttributes} from "../scripts/eventdef";
+import * as Localizer from "../scripts/localizer";
 
 export var info: EventAttributes = {
     name: "ready",
@@ -11,22 +12,31 @@ export var info: EventAttributes = {
 export function execute(client: Client) {
     LogChamp.info("Creative Bot has been started! Using environment: " + process.env.NODE_ENV);
     if(process.env.NODE_ENV !== "maintenance") {
-        if(client.user !== null) {
-            client.user.setPresence({
-                status: "online",
-                activity: {name: "Use '" + config.prefix + "help' to learn what I can do!"}
-            });
-        }
+        startNormally(client);
     }
     else {
-        if(client.user !== null) {
-            client.user.setPresence({
-                status: "dnd",
-                activity: {name: "Bot is currently undergoing maintenance!"}
-            });
-        }
+        startInMaintenanceMode(client);
     }
     if(process.env.NODE_ENV == "development" || process.env.NODE_ENV == "build") {
         config.prefix = "dev ";
+    }
+}
+
+function startNormally(client: Client): void {
+    if(client.user !== null) {
+        client.user.setPresence({
+            status: "online",
+            activity: {name: "Use '" + config.prefix + "help' to learn what I can do!"}
+        });
+    }
+    Localizer.initializeLocalizer();
+}
+
+function startInMaintenanceMode(client: Client): void {
+    if(client.user !== null) {
+        client.user.setPresence({
+            status: "dnd",
+            activity: {name: "Bot is currently undergoing maintenance!"}
+        });
     }
 }
