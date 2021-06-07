76628c60fc427ea9403fd56f639ba437a77819df
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-07 15:40:24 +0200

    Removed tests for formatting with mock objects

--- src/commands/fun/gameidea.test.ts
+++ src/commands/fun/gameidea.test.ts
@@ -2,26 +2,10 @@ import * as GameIdea from "./gameidea";
 
 var gameIdeaCommand = new GameIdea.GameIdeaCommand();
 
-var gameIdeaCommandWithoutFormatters = new GameIdea.GameIdeaCommand();
-class MockFormatter extends GameIdea.BaseFormatter {
-    format() {
-        return "Mock";
-    }
-}
-
-beforeAll(() => {
-    gameIdeaCommandWithoutFormatters.formatters = [];
-});
-
 test("Processes the word 'Apple' and returns 'An'", () => {
     expect(gameIdeaCommand.getCorrectFormOfArticle("Apple")).toBe("An");
 });
 
 test("Processes the word 'Mango' and returns 'A'", () => {
     expect(gameIdeaCommand.getCorrectFormOfArticle("Mango")).toBe("A");
-});
-
-test("Adds a formatter to command and tests returned string", () => {
-    gameIdeaCommandWithoutFormatters.formatters = [new MockFormatter(gameIdeaCommandWithoutFormatters)];
-    expect(gameIdeaCommandWithoutFormatters.generateGameIdea()).toBe("**Game Idea:** \nMock");
 });
