ec2981de703809065db33bf4ca4c0be8ec79281d
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-07 22:37:21 +0200

    Updated tests to access functions outside of command class now

--- src/commands/fun/gameidea.test.ts
+++ src/commands/fun/gameidea.test.ts
@@ -1,11 +1,9 @@
 import * as GameIdea from "./gameidea";
 
-var gameIdeaCommand = new GameIdea.GameIdeaCommand();
-
 test("Processes the word 'Apple' and returns 'An'", () => {
-    expect(gameIdeaCommand.getCorrectFormOfArticle("Apple")).toBe("An");
+    expect(GameIdea.getCorrectFormOfArticle("Apple")).toBe("An");
 });
 
 test("Processes the word 'Mango' and returns 'A'", () => {
-    expect(gameIdeaCommand.getCorrectFormOfArticle("Mango")).toBe("A");
+    expect(GameIdea.getCorrectFormOfArticle("Mango")).toBe("A");
 });
