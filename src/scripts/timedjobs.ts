8306a5d8d609f862a8620cdbfe9b52b438e4300e
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-27 23:42:48 +0200

    Added job for resetting tokens at the beginning of every month

--- src/scripts/timedjobs.ts
+++ src/scripts/timedjobs.ts
@@ -5,7 +5,14 @@ export function scheduleTimedJobs() {
     const vouchTokenResetRule = new Scheduler.RecurrenceRule();
     vouchTokenResetRule.tz = "Etc/GMT";
     vouchTokenResetRule.hour = 0;
-    Scheduler.scheduleJob(vouchTokenResetRule, async function() {
+    const vouchTokenResetJob = Scheduler.scheduleJob(vouchTokenResetRule, async function() {
         await TTA.resetVouchTokens();
+    });
+
+    const tokenAndRoleResetRule = new Scheduler.RecurrenceRule();
+    tokenAndRoleResetRule.tz = "Etc/GMT";
+    tokenAndRoleResetRule.date = 1;
+    const tokenAndRoleResetJob = Scheduler.scheduleJob(tokenAndRoleResetRule, async function() {
+        await TTA.resetTokens();
     });
 }
