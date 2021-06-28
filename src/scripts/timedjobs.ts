89ead5b34d71c55d146d1ed8c7c8521468169aa9
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-28 20:26:28 +0200

    Implemented removal of the Distinguished Member role at the beginning of each month

--- src/scripts/timedjobs.ts
+++ src/scripts/timedjobs.ts
@@ -1,5 +1,7 @@
 import * as Scheduler from "node-schedule";
 import * as TTA from "./tokentableaccessor";
+import {client} from "../index";
+import {GuildMember} from "discord.js";
 
 export function scheduleTimedJobs() {
     const vouchTokenResetRule = new Scheduler.RecurrenceRule();
@@ -14,5 +16,21 @@ export function scheduleTimedJobs() {
     tokenAndRoleResetRule.date = 1;
     const tokenAndRoleResetJob = Scheduler.scheduleJob(tokenAndRoleResetRule, async function() {
         await TTA.resetTokens();
+        removeTopEarnerRoles();
     });
+}
+
+function removeTopEarnerRoles() {
+    var creativeGuildID = "765328952495046696";
+    var distinguishedMemberRoleID = "767423198957928448";
+
+    var creativeGuild = client.guilds.cache.get(creativeGuildID);
+    var membersWithRole = creativeGuild?.members.cache.filter((member: GuildMember) => {
+        return member.roles.cache.has(distinguishedMemberRoleID);
+    });
+
+    if(membersWithRole === undefined) return;
+    for(let member of membersWithRole) {
+        member[1].roles.remove(distinguishedMemberRoleID);
+    }
 }
