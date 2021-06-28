2541a0ccb758b97ddd75ce83f2d78584f51a8a38
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-28 20:45:53 +0200

    Moved responsibility of adding and removing the top earner role

---
+++ src/scripts/tokensystem/rolemanager.ts
@@ -0,0 +1,23 @@
+import {client} from "../../index";
+import {GuildMember} from "discord.js";
+
+var creativeGuildID = "765328952495046696";
+var distinguishedMemberRoleID = "767423198957928448";
+var creativeGuild = client.guilds.cache.get(creativeGuildID);
+
+export async function addRoleToTopEarner(userID: bigint): Promise<void> {
+    var topEarner = creativeGuild?.members.cache.get(userID.toString());
+    if(topEarner === undefined) return;
+    topEarner.roles.add(distinguishedMemberRoleID);
+}
+
+export async function removeTopEarnerRoles(): Promise<void> {
+    var membersWithRole = creativeGuild?.members.cache.filter((member: GuildMember) => {
+        return member.roles.cache.has(distinguishedMemberRoleID);
+    });
+
+    if(membersWithRole === undefined) return;
+    for(let member of membersWithRole) {
+        await member[1].roles.remove(distinguishedMemberRoleID);
+    }
+}
