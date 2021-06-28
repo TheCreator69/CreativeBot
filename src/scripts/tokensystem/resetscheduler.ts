6c2d28d68a3d333d1240dd1b9c6bcb458433d66f
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-06-28 20:45:40 +0200

    Moved and renamed timedjobs.ts to resetscheduler.ts

---
+++ src/scripts/tokensystem/resetscheduler.ts
@@ -0,0 +1,20 @@
+import * as Scheduler from "node-schedule";
+import * as TTA from "../tokentableaccessor";
+import * as RoleManager from "./rolemanager";
+
+export function scheduleTimedJobs() {
+    const vouchTokenResetRule = new Scheduler.RecurrenceRule();
+    vouchTokenResetRule.tz = "Etc/GMT";
+    vouchTokenResetRule.hour = 0;
+    const vouchTokenResetJob = Scheduler.scheduleJob(vouchTokenResetRule, async function() {
+        await TTA.resetVouchTokens();
+    });
+
+    const tokenAndRoleResetRule = new Scheduler.RecurrenceRule();
+    tokenAndRoleResetRule.tz = "Etc/GMT";
+    tokenAndRoleResetRule.date = 1;
+    const tokenAndRoleResetJob = Scheduler.scheduleJob(tokenAndRoleResetRule, async function() {
+        await TTA.resetTokens();
+        await RoleManager.removeTopEarnerRoles();
+    });
+}
