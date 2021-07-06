bb0e2add8d4a737e3ce85a08df9f051f61f752e4
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-07-06 16:33:23 +0200

    Removed different handling of messages based on environment

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,6 +1,6 @@
 import * as config from "../config.json";
 import * as AdminCheck from "../scripts/admincheck";
-import {commands, client} from "../index";
+import {commands} from "../index";
 import {Message, Client} from "discord.js";
 import {EventAttributes} from "../scripts/eventdef";
 import * as Localizer from "../scripts/localizer";
@@ -10,47 +10,8 @@ export var info: EventAttributes = {
 }
 
 export async function execute(client: Client, message: Message): Promise<void> {
-    await handleMessageInDifferentEnvironments(message);
-}
-
-async function handleMessageInDifferentEnvironments(message: Message): Promise<void> {
-    if(process.env.NODE_ENV == "production") {
-        handleMessageInProduction(message);
-    }
-    else if(process.env.NODE_ENV == "development" || process.env.NODE_ENV == "build") {
-        handleMessageInDevAndBuild(message);
-    }
-    else if(process.env.NODE_ENV == "maintenance") {
-        handleMessageInMaintenance(message);
-    }
-}
-
-async function handleMessageInProduction(message: Message): Promise<void> {
     if(message.content.startsWith(config.prefix) && !message.author.bot) {
         await executeCommandIfPossible(message);
-    }
-}
-
-async function handleMessageInDevAndBuild(message: Message): Promise<void> {
-    if(message.content.startsWith(config.prefix) && !message.author.bot) {
-        if(await AdminCheck.checkIfUserIsAdmin(BigInt(message.author.id))) {
-            if(message.channel.type == "dm") {
-                await executeCommandIfPossible(message);
-            }
-            else if(message.guild && message.guild.available && message.guild.id == config.testServerID) {
-                await executeCommandIfPossible(message);
-            }
-        }
-    }
-}
-
-async function handleMessageInMaintenance(message: Message): Promise<void> {
-    if(message.content.startsWith(config.prefix) && !message.author.bot) {
-        var commandInfo = getCommandInfoFromMessage(message);
-
-        if(await canCommandBeExecuted(message, commandInfo)) {
-            message.channel.send(Localizer.translate("messageEvent.maintenanceReply"));
-        }
     }
 }
 
