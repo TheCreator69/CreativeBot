933d63304c9d3e6b0582460f3dff2ca4abc5dd71
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2021-07-12 00:36:15 +0200

    Change name to be more generic and add job to grant role after 7 days

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import {Client} from "discord.js";
 import * as config from "../config.json";
 import * as LogChamp from "../scripts/logchamp";
 import {EventAttributes} from "../scripts/def/eventdef";
-import * as TokenResetter from "../scripts/tokensystem/resetscheduler";
+import * as TimedEventScheduler from "../scripts/tokensystem/timedeventscheduler";
 
 export var info: EventAttributes = {
     name: "ready",
@@ -16,6 +16,6 @@ export function execute(client: Client) {
             status: "online",
             activity: {name: "Use '" + config.prefix + "help' to learn what I can do!"}
         });
-        TokenResetter.scheduleTimedJobs();
+        TimedEventScheduler.scheduleTimedJobs();
     }
 }
--- src/scripts/tokensystem/timedeventscheduler.ts
+++ src/scripts/tokensystem/timedeventscheduler.ts
@@ -1,6 +1,7 @@
 import * as Scheduler from "node-schedule";
 import * as TTA from "../database/tokentableaccessor";
 import * as RoleManager from "./rolemanager";
+import {client} from "../../index";
 
 export function scheduleTimedJobs() {
     const vouchTokenResetRule = new Scheduler.RecurrenceRule();
@@ -16,5 +17,18 @@ export function scheduleTimedJobs() {
     const tokenAndRoleResetJob = Scheduler.scheduleJob(tokenAndRoleResetRule, async function() {
         await TTA.resetTokens();
         await RoleManager.removeTopEarnerRoles();
+    });
+
+    const distinguishedRoleAwardRule = new Scheduler.RecurrenceRule();
+    tokenAndRoleResetRule.tz = "Etc/GMT";
+    tokenAndRoleResetRule.date = 7;
+    const distinguishedRoleAwardJob = Scheduler.scheduleJob(tokenAndRoleResetRule, async function() {
+        const topUserEntries = await TTA.getTopTenUsers();
+        for(let i = 0; i < topUserEntries.length; i++) {
+            let userEntry = topUserEntries[i];
+            let user = await client.users.fetch(userEntry.userID.toString());
+            if(user === undefined) break;
+            await RoleManager.addRoleToTopEarner(BigInt(user.id));
+        }
     });
 }
