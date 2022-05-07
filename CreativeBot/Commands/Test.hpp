0185a1e0af9cdcfaf53053a1bc63fbc9e33630eb
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2022-05-07 13:00:55 +0200

    Revert "Split Test.hpp into two files for better clarity"
    This reverts commit 399a0553a942222f71342d22c00e9e5c60d81086.
    
    Made a mistake aaaaa

--- CreativeBot/Commands/Test.hpp
+++ CreativeBot/Commands/Test.hpp
@@ -8,10 +8,32 @@ namespace DiscordCoreAPI
 	class Test : public DiscordCoreAPI::BaseFunction
 	{
 	public:
-		Test();
+		Test()
+		{
+			this->commandName = "test";
+			this->helpDescription = "This is a test description.";
+			DiscordCoreAPI::EmbedData msgEmbed;
+			msgEmbed.setDescription("------\nSimply enter /test!\n------");
+			msgEmbed.setTitle("__**Test Usage:**__");
+			msgEmbed.setTimeStamp(getTimeAndDate());
+			msgEmbed.setColor("FeFeFe");
+			this->helpEmbed = msgEmbed;
+		}
 
-		std::unique_ptr<DiscordCoreAPI::BaseFunction> create();
+		std::unique_ptr<DiscordCoreAPI::BaseFunction> create()
+		{
+			return std::make_unique<Test>();
+		}
 
-		virtual void execute(DiscordCoreAPI::BaseFunctionArguments& args);
+		virtual void execute(DiscordCoreAPI::BaseFunctionArguments& args)
+		{
+			DiscordCoreAPI::InputEvents::deleteInputEventResponseAsync(args.eventData);
+
+			DiscordCoreAPI::RespondToInputEventData dataPackage{args.eventData};
+			dataPackage.addContent("Test Message!");
+			dataPackage.setResponseType(DiscordCoreAPI::InputEventResponseType::Interaction_Response);
+
+			DiscordCoreAPI::InputEvents::respondToEventAsync(dataPackage);
+		}
 	};
 }
